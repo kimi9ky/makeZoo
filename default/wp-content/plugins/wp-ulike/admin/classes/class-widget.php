3a4787904095855ac74815f8b3732460ff4f8084
lizheng <lizheng5277@hotmail.com>
2016-07-10 22:10:17 +0800

    test

--- default/wp-content/plugins/wp-ulike/admin/classes/class-widget.php
+++ default/wp-content/plugins/wp-ulike/admin/classes/class-widget.php
@@ -55,10 +55,11 @@ class wp_ulike_widget extends WP_Widget {
 			$post_count = $post->meta_value;
 			
 			echo $settings['before_item'];
-			echo "<div onclick='window.location.href="' . $permalink . '" title="' . $post_title.'"; return false' class='wp_ulike_imgcontainer' style='background-image:url(\"";
+			echo "<div onclick='window.location.href=" . $permalink . "'>"
+			echo "<div class='wp_ulike_imgcontainer' style='background-image:url(\"";
             echo $settings['show_thumb'] == '1' ? $this->get_post_thumbnail_src($post->ID, $settings['sizeOf']) : '';
             echo "\")'>";
-			echo "</div>";
+			echo "</div></div>";
 
 			echo "<div>"; 
 			echo '<p><a href="' . $permalink . '" title="' . $post_title.'" rel="nofollow">'. $post_title. '</a></p>';
