a53de5ed9c2b5bb206f9f5f8ecdbfff729a523c5
lizheng <lizheng5277@hotmail.com>
2016-07-10 23:27:28 +0800

    roll back

--- default/wp-content/plugins/wp-ulike/admin/classes/class-widget.php
+++ default/wp-content/plugins/wp-ulike/admin/classes/class-widget.php
@@ -55,12 +55,11 @@ class wp_ulike_widget extends WP_Widget {
 			$post_count = $post->meta_value;
 
 			echo $settings['before_item'];
-			echo '<div onclick="windows.location.href=' . $permalink . '">'
 			echo "<div class='wp_ulike_imgcontainer' style='background-image:url(\"";
             echo $settings['show_thumb'] == '1' ? $this->get_post_thumbnail_src($post->ID, $settings['sizeOf']) : '';
             echo "\")'>";
-			echo "</div>";
-			echo "</div>";
+			echo '<a href="' . $permalink . '" title="' . $post_title.'" rel="nofollow">';
+			echo "</div></a>";
 
 			echo "<div>"; 
 			echo '<p><a href="' . $permalink . '" title="' . $post_title.'" rel="nofollow">'. $post_title. '</a></p>';
