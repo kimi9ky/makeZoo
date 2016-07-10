746822cb91105e2a0e4694ffd1bd14d43090ad0f
lizheng <lizheng5277@hotmail.com>
2016-07-10 19:51:59 +0800

    test

--- default/wp-content/plugins/wp-ulike/admin/classes/class-widget.php
+++ default/wp-content/plugins/wp-ulike/admin/classes/class-widget.php
@@ -56,9 +56,9 @@ class wp_ulike_widget extends WP_Widget {
 			
 			echo $settings['before_item'];
 			echo '<a href="' . $permalink . '" title="' . $post_title.'" rel="nofollow">';
-			echo "<img class='wp_ulike_imgcontainer' src='\"";
+			echo "<img src='/";
             echo $settings['show_thumb'] == '1' ? $this->get_post_thumbnail_src($post->ID, $settings['sizeOf']) : '';
-            echo "\"'/>";
+            echo "'/>";
 			echo "</a>";
 
 			echo "<div>"; 
