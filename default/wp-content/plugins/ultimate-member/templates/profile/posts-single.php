1bd7a8f3690c2429412f2767e0ee2985398b0319
kimi9ky <kimi9ky@gmail.com>
2016-07-31 15:33:40 +0800

    no message

--- default/wp-content/plugins/ultimate-member/templates/profile/posts-single.php
+++ default/wp-content/plugins/ultimate-member/templates/profile/posts-single.php
@@ -4,7 +4,7 @@
 			<div class="um-item-link">
 				<i class="um-icon-ios-paper"></i>
 				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-				<a href="<?php do_action('gform_update_post/setup_form', $post_id);?>">修改</a>
+				<a href="<?php apply_filters('gform_update_post/edit_url', $post_id, home_url('/edit_post/'));?>">修改</a>
 			</div>
 			
 			<?php if ( has_post_thumbnail( $post_id ) ) {
