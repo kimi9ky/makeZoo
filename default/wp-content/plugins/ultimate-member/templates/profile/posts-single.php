20d78cbea50b4f5f889a184fb9aee228e016408b
kimi9ky <kimi9ky@gmail.com>
2016-07-31 20:56:31 +0800

    123

--- default/wp-content/plugins/ultimate-member/templates/profile/posts-single.php
+++ default/wp-content/plugins/ultimate-member/templates/profile/posts-single.php
@@ -23,18 +23,17 @@
 		<div class="um-item">
 			<div class="um-item-link link-area">
 				<i class="um-icon-ios-paper"></i>
-				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></br>
-				<a href="<?php echo apply_filters('gform_update_post/edit_url', $post_id, home_url('/?page_id=477/'));?>"><div class="modifyTab">编辑</div></a>
-				<span style="width:25px;"></span>
-				<?php
-					$url = get_bloginfo('url');
-					if (current_user_can('edit_post', $post_id)){
-						echo '<a  href="';
-						echo wp_nonce_url("$url/wp-admin/post.php?action=delete&post=$id", 'delete-post_' . $post_id);
-						echo '"><div class="modifyTab">删除</div></a>';
-					}
+				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></br>				<?php
+				$url = get_bloginfo('url');
+				if (current_user_can('edit_post', $post_id)){
+					echo '<a class="delete-post" href="';
+					echo wp_nonce_url("$url/wp-admin/post.php?action=delete&post=$post_id", 'delete-post_' . $post_id);
+					echo '"><div class="modifyTab">删除</div></a>';
+				}
 				?>
-		</div>
+				<span style="width:25px;"></span>
+				<a href="<?php echo apply_filters('gform_update_post/edit_url', $post_id, home_url('/?page_id=477/'));?>"><div class="modifyTab">编辑</div></a>
+			</div>
 
 			<?php if ( has_post_thumbnail( $post_id ) ) {
 					$image_id = get_post_thumbnail_id( $post_id );
