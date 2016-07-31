1e99cc4703899a0cee8974344cc686315397414a
kimi9ky <kimi9ky@gmail.com>
2016-07-31 13:52:06 +0800

    no message

--- default/wp-content/plugins/ultimate-member/templates/profile/posts-single.php
+++ default/wp-content/plugins/ultimate-member/templates/profile/posts-single.php
@@ -1,7 +1,11 @@
 	<?php while ($ultimatemember->shortcodes->loop->have_posts()) { $ultimatemember->shortcodes->loop->the_post(); $post_id = get_the_ID(); ?>
 
 		<div class="um-item">
-			<div class="um-item-link"><i class="um-icon-ios-paper"></i><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></div>
+			<div class="um-item-link">
+				<i class="um-icon-ios-paper"></i>
+				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+				<a hred="<?php do_action('gform_update_post/setup_form', $post->ID);?>"></a>
+			</div>
 			
 			<?php if ( has_post_thumbnail( $post_id ) ) {
 					$image_id = get_post_thumbnail_id( $post_id );
@@ -16,7 +20,6 @@
 				<span><?php echo sprintf(__('%s ago','ultimatemember'), human_time_diff( get_the_time('U'), current_time('timestamp') ) ); ?></span>
 				<span>in: <?php the_category( ', ' ); ?></span>
 				<span><?php comments_number( __('no comments','ultimatemember'), __('1 comment','ultimatemember'), __('% comments','ultimatemember') ); ?></span>
-				<span><?php ?>123123</span>
 			</div>
 		</div>
 		
